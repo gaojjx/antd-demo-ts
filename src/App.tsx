31126eacaaef3271c4293c6decd17508280c3926
suixin <suixin0309@163.com>
2023-05-11 16:12:03 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import type { FC } from 'react';
+import { Button, ConfigProvider } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
 
-function App() {
+const App:FC = () =>{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
-
-export default App;
+export default ()=>(
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary:'#00b96b',
+      }
+    }}
+    >
+    <App />
+  </ConfigProvider>
+);
