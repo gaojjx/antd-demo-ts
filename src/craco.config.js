a614889a4366528b1d18797e86df5ccf55ca58d6
gaojjx <gaojjx@gmail.com>
2024-04-30 16:29:18 +0800

    improve

--- src/craco.config.js
+++ src/craco.config.js
@@ -1,4 +1,52 @@
 /* craco.config.js */
+const CracoAlias = require('craco-alias');
+
 module.exports = {
-    // ...
-  };
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+  ],
+  style: {
+    postcss: {
+      plugins: [
+        require('autoprefixer'),
+        ...(process.env.NODE_ENV === 'production'
+          ? [require('cssnano')]
+          : []),
+      ],
+    },
+  },
+  webpack: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+    configure: (webpackConfig) => {
+      webpackConfig.optimization.splitChunks = {
+        ...webpackConfig.optimization.splitChunks,
+        cacheGroups: {
+          ...webpackConfig.optimization.splitChunks.cacheGroups,
+          vendor: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name(module, chunks, cacheGroupKey) {
+              const moduleFileName = module
+                .identifier()
+                .split('/')
+                .reduceRight(item => item);
+              const allChunksNames = chunks.map((item) => item.name).join('~');
+              return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
+            },
+            chunks: 'all',
+          },
+        },
+      };
+
+      return webpackConfig;
+    },
+  },
+};
