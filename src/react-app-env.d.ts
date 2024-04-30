fd57eb4fe2676301037394dd713f4d829c36b1e0
gaojjx <gaojjx@gmail.com>
2024-04-30 16:29:13 +0800

    improve

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,26 @@
-/// <reference types="react-scripts" />
+import React, { FC, useState } from 'react';
+
+interface Props {
+  initialValue: string;
+}
+
+const MyComponent: FC<Props> = ({ initialValue }) => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={value} onChange={handleChange} />
+      <p>You entered: {value}</p>
+    </div>
+  );
+};
+
+MyComponent.defaultProps = {
+  initialValue: '',
+};
+
+export default MyComponent;
